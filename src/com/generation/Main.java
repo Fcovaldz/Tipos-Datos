af90b714c62cdc33fdc10afa44a83b3e29042c13
Francisco <franciscobanuelosvaladez@gmail.com>
2021-10-11 19:40:54 -0500

    Initial commit

---
+++ src/com/generation/Main.java
@@ -0,0 +1,44 @@
+package com.generation;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Variable string(cadena).
+        String nombre = "Francisco";
+
+        // Datos primitivos numéricos.
+        byte num1 = 10;
+        short num2 = 10;
+        int num3 = 10;
+        long num4 = 10;
+
+        // Datos primitivos decimales.
+        float decimal1 = 10.121212f;
+        double decimal2 = 10.121212f;
+
+        // Dato primitivo char.
+        char caracter = 'f';
+
+        // Dato primitivo boolean
+        boolean verdadero = true;
+        boolean falso = false;
+        System.out.println(nombre);
+
+        //
+
+        int numero1 = 55;
+        int numero2 = 77;
+
+        int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int multiplicacion = numero1 * numero2;
+        float division = (float) numero1 / numero2;
+
+        System.out.println(suma);
+        System.out.println(resta);
+        System.out.println(multiplicacion);
+        System.out.println(division);
+
+    }
+}
